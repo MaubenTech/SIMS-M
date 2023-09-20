84ea3c2df96016c0c472d5ffde52c81de034f983
Deborah Odungweru <deborah.odungweru@gmail.com>
2023-09-20 23:59:00 +0100

    Test model classes functionality

--- backend/src/main/java/com/ixorasoftware/sims/SimsApplication.java
+++ backend/src/main/java/com/ixorasoftware/sims/SimsApplication.java
@@ -1,10 +1,36 @@
 package com.ixorasoftware.sims;
 
+import com.ixorasoftware.sims.user.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SimsApplication {
+
+	@Bean
+	CommandLineRunner run(UserRepository userRepository,
+						  RoleRepository roleRepository,
+						  PermissionRepository permissionRepository)
+	{
+		return args -> {
+			Permission p1 = new Permission("READ_STUDENT");
+			Permission p2 = new Permission("WRITE_STUDENT");
+			permissionRepository.save(p1);
+			permissionRepository.save(p2);
+
+			Role r1 = new Role("ROLE_ADMIN", List.of(p1, p2));
+			roleRepository.save(r1);
+
+			User u1 = new User("Jane", "Constance", "Doe",
+							  "janedoe@gmail.com", Gender.FEMALE,"secret");
+
+			userRepository.save(u1);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimsApplication.class, args);
