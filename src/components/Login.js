e5380baa61238eff524583d14b8c5bd584870a96
Noah-V <noahvikoo3@gmail.com>
2023-10-04 02:16:03 +0100

    Noah's Commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import "../css/login.css";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import CarouselItem from "./CarouselItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -16,8 +16,8 @@ export const Login = () => {
   const userRef = useRef();
   const errorRef = useRef();
 
-  const [user, setUser] = useState("");
-  const [validName, setValidName] = useState(false);
+  const [username, setUserName] = useState("");
+  const [validUserName, setValidUserName] = useState(false);
   const [userFocus, setUserFocus] = useState(false);
   const [password, setPassword] = useState("");
   const [validPassword, setValidPassword] = useState(false);
@@ -54,16 +54,16 @@ export const Login = () => {
     setActiveIndex(newIndex);
   };
 
-  useEffect(() => {
-    userRef.current.focus();
-  }, []);
+  // useEffect(() => {
+  //   userRef.current.focus();
+  // }, []);
 
   useEffect(() => {
-    const result = USER_REGEX.test(user);
+    const result = USER_REGEX.test(username);
     console.log(result);
-    console.log(user);
-    setValidName(result);
-  }, [user]);
+    console.log(username);
+    setValidUserName(result);
+  }, [username]);
 
   useEffect(() => {
     const result = PASSWORD_REGEX.test(password);
@@ -74,172 +74,212 @@ export const Login = () => {
 
   useEffect(() => {
     setErrorMessage("");
-  }, [user, password, matchPassword]);
+  }, [username, password]);
 
-  const handleSubmit = () => {};
+  const handleSubmit = (event) => {
+    console.log("function start");
+    event.preventDefault();
+    // const userCheck = USER_REGEX.test(username);
+    // const passwordCheck = PASSWORD_REGEX.test(password);
+    // if (!userCheck || !passwordCheck) {
+    //   console.log(username, password);
+    //   setErrorMessage("Invalid Entry");
+    //   return;
+    // }
+    if (username === "Noahvikoo" && password === "Vicecity1@") {
+      setSuccess(true);
+    } else {
+      setErrorMessage("Invalid Details");
+    }
+  };
 
   return (
-    <div className="login-container">
-      <div className="login-form">
-        <div className="left-pane">
-          <div
-            className="left-pane-inner"
-            style={{ transform: `translate(-${activeIndex * 100}%)` }}
-          >
-            {carouselItems.map((item) => {
-              return <CarouselItem item={item} width={"100%"} />;
-            })}
-          </div>
-          <div className="carousel-control">
-            <button
-              className="arrow-button back"
-              onClick={() => updateIndex(activeIndex - 1)}
-            >
-              <span className="material-symbols-outlined">
-                arrow_back_ios_new
-              </span>
-            </button>
-            <div className="carousel-indicator">
-              {carouselItems.map((item, index) => {
-                return (
-                  <button
-                    className="indicators"
-                    onClick={() => updateIndex(index)}
-                  >
-                    <span
-                      className={`material-symbols-outlined ${
-                        index === activeIndex ? "dot active" : "dot"
-                      }`}
-                    >
-                      radio_button_checked
-                    </span>
-                  </button>
-                );
-              })}
-            </div>
-            <button
-              className="arrow-button forward"
-              onClick={() => updateIndex(activeIndex + 1)}
-            >
-              <span className="material-symbols-outlined">
-                arrow_forward_ios
-              </span>
-            </button>
-          </div>
-          <div className="left-pane-overlay"></div>
-        </div>
-        <div className="right-pane">
-          <h3>SIMS</h3>
-          <div className="welcome">
-            <h3>Welcome to SIMS</h3>
-          </div>
-          <div className="error">
-            <p
-              ref={errorRef}
-              className={errorMessage ? "errorMessage" : "offscreen"}
-              aria-live={"assertive"}
-            ></p>
-          </div>
-          <form className="login" onSubmit={handleSubmit}>
-            <div className="username-container">
-              <label htmlFor="username">
-                Username
-                <span className={validName ? "valid" : "hide"}>
-                  <FontAwesomeIcon icon={faCheck} />
-                </span>
-                <span className={validName || !user ? "hide" : "invalid"}>
-                  <FontAwesomeIcon icon={faTimes} />
-                </span>
-              </label>
-              <input
-                type="text"
-                placeholder="John Doe"
-                id="username"
-                ref={userRef}
-                autoComplete="off"
-                onChange={(event) => setUser(event.target.value)}
-                required
-                aria-invalid={validName ? "false" : "true"}
-                aria-describedby="uidnote"
-                onFocus={() => setUserFocus(true)}
-                onBlur={() => setUserFocus(false)}
-              />
-              <p
-                id="uidnote"
-                className={
-                  userFocus && user && !validName ? "instructions" : "offscreen"
-                }
+    <>
+      {success ? (
+        <Navigate to="/learning-center/dashboard" replace={true} />
+      ) : (
+        <div className="login-container">
+          <div className="login-form">
+            <div className="left-pane">
+              <div
+                className="left-pane-inner"
+                style={{ transform: `translate(-${activeIndex * 100}%)` }}
               >
-                <FontAwesomeIcon icon={faInfoCircle} />
-                4 to 24 Characters.
-                <br />
-              </p>
-            </div>
-            <div className="password-container">
-              <label htmlFor="password">
-                Password
-                <span className={validPassword ? "valid" : "hide"}>
-                  <FontAwesomeIcon icon={faCheck} />
-                </span>
-                <span
-                  className={validPassword || !password ? "hide" : "invalid"}
+                {carouselItems.map((item) => {
+                  return <CarouselItem item={item} width={"100%"} />;
+                })}
+              </div>
+              <div className="carousel-control">
+                <button
+                  className="arrow-button back"
+                  onClick={() => updateIndex(activeIndex - 1)}
                 >
-                  <FontAwesomeIcon icon={faTimes} />
-                </span>
-              </label>
-              <input
-                type="password"
-                placeholder="********"
-                id="password"
-                onChange={(event) => setPassword(event.target.value)}
-                // value={password}
-                required
-                aria-invalid={validPassword ? "false" : "true"}
-                aria-describedby="password-instruction"
-                onFocus={() => setPasswordFocus(true)}
-                onBlur={() => setPasswordFocus(false)}
-              />
-              <p
-                id="password-instruction"
-                className={
-                  passwordFocus && !validPassword ? "instructions" : "offscreen"
-                }
-              >
-                <FontAwesomeIcon icon={faInfoCircle} />8 to 24 Characters, One
-                UpperCase letter and at least one special character <br />
-              </p>
+                  <span className="material-symbols-outlined">
+                    arrow_back_ios_new
+                  </span>
+                </button>
+                <div className="carousel-indicator">
+                  {carouselItems.map((item, index) => {
+                    return (
+                      <button
+                        className="indicators"
+                        onClick={() => updateIndex(index)}
+                      >
+                        <span
+                          className={`material-symbols-outlined ${
+                            index === activeIndex ? "dot active" : "dot"
+                          }`}
+                        >
+                          radio_button_checked
+                        </span>
+                      </button>
+                    );
+                  })}
+                </div>
+                <button
+                  className="arrow-button forward"
+                  onClick={() => updateIndex(activeIndex + 1)}
+                >
+                  <span className="material-symbols-outlined">
+                    arrow_forward_ios
+                  </span>
+                </button>
+              </div>
+              <div className="left-pane-overlay"></div>
             </div>
-            <div className="role-container">
-              <select name="role" id="" placeholder="Role">
-                <option value="proprietor">Proprietor</option>
-                <option value="admin">Admin</option>
-                <option value="teacher">Teacher</option>
-                <option value="student">Student</option>
-                <option value="parent">Parent</option>
-              </select>
-            </div>
-            <div className="forgot-password">
-              <Link className="forgotPW">Forgot your password?</Link>
-            </div>
-            <div className="sign-in">
-              {/* <button
+            <div className="right-pane">
+              <h3>SIMS</h3>
+              <div className="welcome">
+                <h3>Welcome to SIMS</h3>
+              </div>
+              <div className="error">
+                <p
+                  ref={errorRef}
+                  className={errorMessage ? "errorMessage" : "offscreen"}
+                  aria-live={"assertive"}
+                >
+                  {errorMessage}
+                </p>
+              </div>
+              <form className="login">
+                <div className="username-container">
+                  <label htmlFor="username">
+                    Username
+                    <span className={validUserName ? "valid" : "hide"}>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span
+                      className={
+                        validUserName || !username ? "hide" : "invalid"
+                      }
+                    >
+                      <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="John Doe"
+                    id="username"
+                    value={username}
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(event) => setUserName(event.target.value)}
+                    required
+                    aria-invalid={validUserName ? "false" : "true"}
+                    aria-describedby="username-instructions"
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                  />
+                  <p
+                    id="username-instructions"
+                    className={
+                      userFocus && username && !validUserName
+                        ? "instructions"
+                        : "offscreen"
+                    }
+                  >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    4 to 24 Characters.
+                    <br />
+                  </p>
+                </div>
+                <div className="password-container">
+                  <label htmlFor="password">
+                    Password
+                    <span className={validPassword ? "valid" : "hide"}>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span
+                      className={
+                        validPassword || !password ? "hide" : "invalid"
+                      }
+                    >
+                      <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="********"
+                    id="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    // value={password}
+                    required
+                    aria-invalid={validPassword ? "false" : "true"}
+                    aria-describedby="password-instructions"
+                    onFocus={() => setPasswordFocus(true)}
+                    onBlur={() => setPasswordFocus(false)}
+                  />
+                  <p
+                    id="password-instructions"
+                    className={
+                      passwordFocus && !validPassword
+                        ? "instructions"
+                        : "offscreen"
+                    }
+                  >
+                    <FontAwesomeIcon icon={faInfoCircle} />8 to 24 Characters,
+                    One UpperCase letter and at least one special character{" "}
+                    <br />
+                  </p>
+                </div>
+                <div className="role-container">
+                  <select name="role" id="" placeholder="Role">
+                    <option value="proprietor">Proprietor</option>
+                    <option value="admin">Admin</option>
+                    <option value="teacher">Teacher</option>
+                    <option value="student">Student</option>
+                    <option value="parent">Parent</option>
+                  </select>
+                </div>
+                <div className="forgot-password">
+                  <Link className="forgotPW">Forgot your password?</Link>
+                </div>
+                <div className="sign-in">
+                  {/* <button
               // className="signinButton"
               // disabled={!validName || !validPassword ? true : false}
               // className={disabled ? "enabledSignin" : "signinButton"}
               >
                 Sign In
               </button> */}
-              {!validName || !validPassword ? (
-                <button className="disableSigninButton">Sign in</button>
-              ) : (
-                <button className="enabledSignIn" disabled={true}>
-                  Sign in
-                </button>
-              )}
+                  {!validUserName || !validPassword ? (
+                    <button className="disableSigninButton">Sign in</button>
+                  ) : (
+                    <button
+                      className="enabledSignIn"
+                      disabled={true}
+                      onClick={() => console.log("clicked")}
+                    >
+                      Sign in
+                    </button>
+                  )}
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
