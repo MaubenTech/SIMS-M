5b3d6a87edea3d637b10c33928bbdb8b2ac0e4c4
KING-REX <anijimjamesmaurice@gmail.com>
2023-11-24 13:17:51 +0100

    Rex's Commit: Negligible changes

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -12,11 +12,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnboardingNavigation from "./OnboardingNavigation";
 import LoginScreen from "../components/loginScreens/LoginScreen";
-import withHomePageWrapper from "../helpers/withHomePageWrapper";
-import HomeScreenSwitch from "../components/homeScreens/HomeScreenSwitch";
 import BottomTabNavigation from "./BottomTabNavigation";
 
 export type RootStackParamList = {
+	BottomTabNavigation: undefined;
+	LoginScreen: undefined;
 	OnboardingScreens: undefined;
 };
 
@@ -25,8 +25,8 @@ const RootNavigation = (): JSX.Element => {
 
 	return (
 		<NavigationContainer>
-			<Navigator initialRouteName="LoginScreen">
-				<Group>
+			<Navigator initialRouteName="BottomTabNavigation">
+				<Group screenOptions={{ headerShown: false }}>
 					<Screen name="LoginScreen" component={LoginScreen} />
 				</Group>
 				<Group>
