6edb327da1e9478eaa1c6d45532b0e35ca396542
KING-REX <anijimjamesmaurice@gmail.com>
2023-11-25 00:03:33 +0100

    Rex's Commit: Setting a dummy role to test switch

--- src/helpers/withHomePageWrapper.tsx
+++ src/helpers/withHomePageWrapper.tsx
@@ -1,21 +1,31 @@
 import { Dimensions, Image, StyleSheet, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import HomePageSalutation from "./HomePageSalutation";
+import { useDispatch, useSelector } from "react-redux";
+import { roleActions } from "../redux/roleSlice";
+import { ReduxStates } from "../redux";
+import ParentContainer from "./ParentContainer";
 
 type HomePageWrapperProps = {};
 
-const withHomePageWrapper = <P extends object>(WrappedComponent: React.ComponentType<P & HomePageWrapperProps>) => {
-  const WithHomePageWrapper = (props: P) => {
-    return (
-      <View>
-        {/* <WrappedComponent {...props} /> */}
-        <HomePageSalutation />
-        <WrappedComponent {...props} />
-      </View>
-    );
-  };
+const withHomePageWrapper = <P extends object>(
+	WrappedComponent: React.ComponentType<P & HomePageWrapperProps>
+) => {
+	const dispatch = useDispatch();
+	dispatch(roleActions.setRole("Teacher"));
+	const role = useSelector((state: ReduxStates) => state.role.name);
+	console.log(`${role}`);
+	const WithHomePageWrapper = (props: P) => {
+		return (
+			<ParentContainer>
+				{/* <WrappedComponent {...props} /> */}
+				<HomePageSalutation />
+				<WrappedComponent {...props} />
+			</ParentContainer>
+		);
+	};
 
-  return WithHomePageWrapper;
+	return WithHomePageWrapper;
 };
 
 const styles = StyleSheet.create({});
