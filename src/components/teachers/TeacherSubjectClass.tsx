33ab0b297857a9b8a35cfa5ab78c2259de57617f
KING-REX <anijimjamesmaurice@gmail.com>
2023-11-25 00:00:43 +0100

    Rex's Commit: Added TeacherSubjectClass component

---
+++ src/components/teachers/TeacherSubjectClass.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ParentContainer from "../../helpers/ParentContainer";
+
+const TeacherSubjectClass = (): JSX.Element => {
+	return (
+		<ParentContainer>
+			<Text>TeacherSubjectClass</Text>
+		</ParentContainer>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {},
+});
+
+export default TeacherSubjectClass;
