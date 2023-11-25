30e6fefce5b84671233747344fead5427dd7bfda
KING-REX <anijimjamesmaurice@gmail.com>
2023-11-25 21:22:46 +0100

    Rex's Commit: Added ParentChildGrades screen

---
+++ src/components/parents/ParentChildGrades.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ParentChildGrades = (): JSX.Element => {
+	return (
+		<View>
+			<Text>ParentChildGrades</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default ParentChildGrades;
