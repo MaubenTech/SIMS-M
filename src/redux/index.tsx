e200188aae7778a642bd5b9fab1a29f920312761
KING-REX <anijimjamesmaurice@gmail.com>
2023-11-25 14:59:22 +0100

    Rex's Commit: Added userSlice to redux store

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import roleSlice, { Role, RoleStateType } from "./roleSlice";
 import authSlice, { AuthStateType } from "./authSlice";
+import userSlice, { UserStateType } from "./userSlice";
 
 export type ReduxStates = {
 	role: RoleStateType;
 	auth: AuthStateType;
+	user: UserStateType;
 };
 
 const store = configureStore({
 	reducer: {
 		role: roleSlice.reducer,
 		auth: authSlice.reducer,
+		user: userSlice.reducer,
 	},
 });
 
